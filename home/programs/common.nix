deaaee1320604ee69daf2070dd1257dcc5ad09ec
dt <john_does@aol.com>
2025-03-05 01:51:32 +0100

    feat: added scrcpy, obs-studio

--- home/programs/common.nix
+++ home/programs/common.nix
@@ -2,10 +2,12 @@
 {
   home.packages = with pkgs; [
     kdePackages.kate
+    kdePackages.kcalc
     megasync
     keepassxc
     obsidian
-
+    scrcpy
+    obs-studio
   ];
   services = {
     # syncthing.enable = true;
