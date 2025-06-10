40e9914033274e23fd6e46d10943446fb8e31813
dt <35782618+detour0@users.noreply.github.com>
2025-06-10 23:48:27 +0200

    chore: update

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -1,14 +1,14 @@
 { pkgs, ... }:
 {
-    programs.neovim = {
-      enable = true;
-      defaultEditor = true;
-      viAlias = true;
-      vimAlias = true;
-      vimdiffAlias = true;
-      plugins = with pkgs.vimPlugins; [
-        comment-nvim
-        hop-nvim
-      ];
-    };
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+    plugins = with pkgs.vimPlugins; [
+      comment-nvim
+      hop-nvim
+    ];
+  };
 }
