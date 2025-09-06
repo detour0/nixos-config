6b62f9adfc7b808d6403c343b7a0b195d1182dd4
dt <35782618+detour0@users.noreply.github.com>
2025-09-06 23:13:55 +0200

    feat: added home-file export nvim config

--- home/programs/nvim.nix
+++ home/programs/nvim.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
 {
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
+  home.file.".config/nvim/init.lua".source = lib.mkForce (
+    config.lib.file.mkOutOfStoreSymlink ./../../dotfiles/nvim/init.lua
+  );
+  home.file.".config/nvim/lua/code_config/vscode_keymap.lua".source = lib.mkForce (
+    config.lib.file.mkOutOfStoreSymlink ./../../dotfiles/nvim/lua/code_config/vscode_keymap.lua
+  );
   programs.neovim = {
     enable = true;
     defaultEditor = true;
