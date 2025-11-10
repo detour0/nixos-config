baa4897d5a7e2858b5902ff638491dd0aa91a202
dt <35782618+detour0@users.noreply.github.com>
2025-11-10 23:39:43 +0100

    chore: clean up old files

--- modules/extrargs.nix
+++
@@ -1,8 +0,0 @@
-{ config, inputs, pkgs, ... }:
-{
-  # This allows you to access `pkgsUnstable` anywhere in your config
-  _module.args.pkgsUnstable = import inputs.nixpkgs-unstable {
-    inherit (pkgs.stdenv.hostPlatform) system;
-    inherit (config.nixpkgs) config;
-  };
-}
