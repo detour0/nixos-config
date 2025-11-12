bed4c1d6465a51804a4badc9d6ed09fb602dde03
dt <35782618+detour0@users.noreply.github.com>
2025-11-12 22:02:14 +0100

    chore: file clean up

--- home/programs/wt.nix
+++
@@ -1,6 +0,0 @@
-{ config, lib, pkgs, ... }:
-{
-  home.packages = [ pkgs.wezterm ];
-  xdg.configFile."wezterm/wezterm.lua".source = 
-    config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/workspace/nixos-config/dotfiles/wezterm.lua";
-}
